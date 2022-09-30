3ff2e8b8ed05892b0aef8f41f098bac1e889afdd
MogliCodes <moglicodes@gmail.com>
2022-09-30 19:25:44 +0200

    feat: provide video component

---
+++ components/video/VideoYoutube.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <iframe class="w-full aspect-video" :src="props.videoSource"></iframe>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+interface Props {
+  videoSource: string;
+}
+
+const props = defineProps<Props>();
+</script>
--- components/wrapper/ElementVideoWrapper.vue
+++ components/wrapper/ElementVideoWrapper.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-    <div>ElementVideo</div>
-    <iframe
-      class="w-full aspect-video"
-      :src="$attrs.videoSource"
-      frameborder="0"
-    ></iframe>
+    <component :is="resolveVideoTypeComponent(props.videoSource)" />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+interface Props {
+  videoSource?: string;
+}
+
+const props = defineProps<Props>();
+
+function resolveVideoTypeComponent(videoSource) {
+  if (videoSource.includes("youtube")) {
+    return resolveComponent("VideoYoutube");
+  }
+}
+</script>
